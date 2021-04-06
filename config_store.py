04eb919047618b3d0aa57e807340f3f7e03a8336
ksanderer <ksanderer@gmail.com>
2021-04-06 18:16:49 +0300

    added config_store primitives to fetch configs

---
+++ config_store.py
@@ -0,0 +1,33 @@
+import logging
+from enum import Enum
+from typing import Optional
+
+from rcdb_commons.schemas.bot import BotConfigResponse
+import requests
+
+
+logger = logging.getLogger("rcdb_config_store")
+
+
+class ConfigException(Exception):
+    pass
+
+
+class ConfigStore:
+    def __init__(self, api_url, token):
+        self.api_url = api_url
+        self.session = requests.Session()
+        self.session.headers.update({'Authorization': f'Bearer {token}'})
+        # self.session.headers.update({'X-CSRFToken': f'1dzy45EaC1LjJp7T67TnOYTCEotlO9aRge1z0Knn31b1ps19RKLFh3CtZHnOrAqd'})
+
+    def get_config(self, bot_id: int) -> BotConfigResponse:
+        r = self.session.get(
+            f"{self.api_url}/bot/{bot_id}",
+            timeout=100
+        )
+        if r.status_code == 200:
+            return BotConfigResponse(**r.json())
+        elif r.status_code == 404:
+            return None
+        else:
+            raise ConfigException(r.text)
